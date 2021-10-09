d6063844f57b7d50ca2ed371be2a6ee72674ba3d
michael.yeo <michael.yeo@temus.com>
2021-10-09 17:39:51 +0800

    Added stylings to Home

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, Image, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  ScrollView,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import colors from '../assets/colors/color';
 import categoriesData from '../assets/data/categoriesData';
+import popularData from '../assets/data/popularData';
 
 Feather.loadFont();
+MaterialCommunityIcons.loadFont();
 
 const Home = () => {
   const renderCategoryItem = ({item}) => {
@@ -14,9 +23,10 @@ const Home = () => {
       <View
         style={[
           styles.categoryItemWrapper,
+          // eslint-disable-next-line react-native/no-inline-styles
           {
             backgroundColor: item.selected ? colors.primary : colors.white,
-            marginLeft: item.id == 1 ? 20 : 0,
+            marginLeft: item.id === 1 ? 20 : 0,
           },
         ]}>
         <Image source={item.image} style={styles.categoryItemImage} />
@@ -41,41 +51,97 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      {/* Header */}
-      <SafeAreaView>
-        <View style={styles.headerWrapper}>
-          <Image
-            source={require('../assets/images/profile.png')}
-            style={styles.profileImage}
-          />
-          <Feather name="menu" size={24} color={colors.textDark} />
-        </View>
-      </SafeAreaView>
-      {/* Titles */}
-      <View style={styles.titlesWrapper}>
-        <Text style={styles.titlesSubtitle}>Food</Text>
-        <Text style={styles.titlesTitle}>Delivery</Text>
-      </View>
-      {/* Search */}
-      <View style={styles.searchWrapper}>
-        <Feather name="search" size={16} color={colors.textDark} />
-        <View style={styles.search}>
-          <Text style={styles.searchText}>Search</Text>
-        </View>
-      </View>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        showsVerticalScrollIndicator={false}>
+        {/* Header */}
+        <SafeAreaView>
+          <View style={styles.headerWrapper}>
+            <Image
+              source={require('../assets/images/profile.png')}
+              style={styles.profileImage}
+            />
+            <Feather name="menu" size={24} color={colors.textDark} />
+          </View>
+        </SafeAreaView>
 
-      {/* catergories */}
-      <View style={styles.categoriesWrapper}>
-        <Text style={styles.categoriesTitle}>Categories</Text>
-        <View style={styles.categoriesListWrapper}>
-          <FlatList
-            data={categoriesData}
-            renderItem={renderCategoryItem}
-            keyExtractor={item => item.id}
-            horizontal={true}
-          />
+        {/* Titles */}
+        <View style={styles.titlesWrapper}>
+          <Text style={styles.titlesSubtitle}>Food</Text>
+          <Text style={styles.titlesTitle}>Delivery</Text>
         </View>
-      </View>
+
+        {/* Search */}
+        <View style={styles.searchWrapper}>
+          <Feather name="search" size={16} color={colors.textDark} />
+          <View style={styles.search}>
+            <Text style={styles.searchText}>Search</Text>
+          </View>
+        </View>
+
+        {/* catergories */}
+        <View style={styles.categoriesWrapper}>
+          <Text style={styles.categoriesTitle}>Categories</Text>
+          <View style={styles.categoriesListWrapper}>
+            <FlatList
+              data={categoriesData}
+              renderItem={renderCategoryItem}
+              keyExtractor={item => item.id}
+              horizontal={true}
+            />
+          </View>
+        </View>
+
+        {/* Popular */}
+        <View style={styles.popularWrapper}>
+          <Text style={styles.popularTitle}>Popular</Text>
+          {popularData.map(item => (
+            <View
+              style={[
+                styles.popularCardWrapper,
+                // eslint-disable-next-line react-native/no-inline-styles
+                {
+                  marginTop: item.id === 1 ? 10 : 20,
+                },
+              ]}>
+              <View>
+                <View>
+                  <View style={styles.popularTopWrapper}>
+                    <MaterialCommunityIcons
+                      name="crown"
+                      size={12}
+                      color={colors.primary}
+                    />
+                    <Text style={styles.popularTopText}>Top of the Week</Text>
+                  </View>
+                  <View style={styles.popularTitleWrapper}>
+                    <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                    <Text style={styles.popularTitlesWeight}>
+                      Weight {item.weight}
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.popularCardBottom}>
+                  <View style={styles.addPizzaButton}>
+                    <Feather name="plus" size={10} color={colors.textDark} />
+                  </View>
+                  <View style={styles.ratingWrapper}>
+                    <MaterialCommunityIcons
+                      name="star"
+                      size={10}
+                      color={colors.textDark}
+                    />
+                    <Text style={styles.rating}>{item.rating}</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.popularCardRight}>
+                <Image source={item.image} style={styles.popularCardImage} />
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -145,6 +211,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5CA48',
     marginRight: 20,
     borderRadius: 20,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 2,
   },
   categoryItemImage: {
     width: 60,
@@ -170,6 +244,84 @@ const styles = StyleSheet.create({
   },
   categorySelectIcon: {
     alignSelf: 'center',
+  },
+  popularWrapper: {
+    paddingHorizontal: 20,
+  },
+  popularTitle: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+  },
+  popularCardWrapper: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    paddingTop: 20,
+    paddingLeft: 20,
+    flexDirection: 'row',
+    overflow: 'hidden',
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 2,
+  },
+  popularTopWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  popularTopText: {
+    marginLeft: 10,
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+  },
+  popularTitleWrapper: {
+    marginTop: 20,
+  },
+  popularTitlesTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 14,
+    color: colors.textDark,
+  },
+  popularTitlesWeight: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 12,
+    color: colors.textLight,
+    marginTop: 5,
+  },
+  popularCardBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: -20,
+  },
+  addPizzaButton: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  ratingWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  rating: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 12,
+    color: colors.textDark,
+    marginLeft: 5,
+  },
+  popularCardRight: {
+    marginLeft: 40,
+  },
+  popularCardImage: {
+    width: 170,
+    height: 120,
+    resizeMode: 'contain',
   },
 });
 
